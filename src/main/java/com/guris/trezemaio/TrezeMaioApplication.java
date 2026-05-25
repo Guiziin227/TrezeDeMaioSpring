6d418d9be188559bca3540b232a3e901ba0db4d5
Guilherme W. Henriques <guilherme.whenriques@ufn.edu.br>
2026-05-25 10:28:19 -0300

    deu

---
+++ src/main/java/com/guris/trezemaio/TrezeMaioApplication.java
@@ -0,0 +1,13 @@
+package com.guris.trezemaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrezeMaioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrezeMaioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guris/trezemaio/TrezeMaioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guris.trezemaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrezeMaioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
