5a72322b75a7ac5a19f2a73bac4f95d0f8ab8168
Guiziin227 <gh63388@gmail.com>
2026-06-01 09:22:37 -0300

    uuid

--- src/main/java/com/guris/trezemaio/model/Usuario.java
+++ src/main/java/com/guris/trezemaio/model/Usuario.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @Entity
@@ -12,8 +14,8 @@ import lombok.Setter;
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     private String name;
     private String password;
