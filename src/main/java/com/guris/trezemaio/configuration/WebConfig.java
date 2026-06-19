73ccff3e0538c0f1783a5a29a984940fad36b75d
OtavioBaggio <otavio.baggiorosul@gmail.com>
2026-06-19 14:51:23 -0300

    polindo o código

--- src/main/java/com/guris/trezemaio/configuration/WebConfig.java
+++ src/main/java/com/guris/trezemaio/configuration/WebConfig.java
@@ -1,4 +1,4 @@
-package com.guris.trezemaio.configuration; // Garanta que o pacote está correto
+package com.guris.trezemaio.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -9,11 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Pega o mesmo caminho do diretório do usuário usado no seu controller
+        // Pega o mesmo caminho do diretório do usuário usado no controller
         String diretorioUsuario = System.getProperty("user.home");
 
         // No Spring, mapeamentos físicos precisam começar com o prefixo "file:"
-        // O "file:" indica ao framework que ele deve buscar no disco rígido do Linux e não dentro do .jar
         String caminhoFisico = "file:" + diretorioUsuario + "/projeto_uploads/";
 
         // Configura o mapeamento URL -> Pasta Física
