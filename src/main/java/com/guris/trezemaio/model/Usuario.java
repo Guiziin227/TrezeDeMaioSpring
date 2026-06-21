2e9c7468c3c66a31a8fc9a15c3c8d755715d961a
Guiziin227 <gh63388@gmail.com>
2026-06-21 11:25:58 -0300

    adicionando auditoria no usuario

--- src/main/java/com/guris/trezemaio/model/Usuario.java
+++ src/main/java/com/guris/trezemaio/model/Usuario.java
@@ -4,13 +4,20 @@ import com.guris.trezemaio.model.enums.TipoUsuario;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "tb_usuario")
+@EntityListeners(AuditingEntityListener.class)
 public class Usuario {
 
     @Id
@@ -22,4 +29,20 @@ public class Usuario {
 
     @Enumerated(EnumType.STRING)
     private TipoUsuario type;
+
+    @CreatedDate
+    @Column(name = "data_criacao", updatable = false)
+    private LocalDateTime dataCriacao;
+
+    @LastModifiedDate
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @CreatedBy
+    @Column(name = "criado_por", updatable = false)
+    private String criadoPor;
+
+    @LastModifiedBy
+    @Column(name = "modificado_por")
+    private String modificadoPor;
 }
