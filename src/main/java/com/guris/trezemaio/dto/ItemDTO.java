2feaa60dc0fe8b29d94ba2e0b280f784c09c0a23
OtavioBaggio <otavio.baggiorosul@gmail.com>
2026-06-25 16:19:43 -0300

    Add anotação de validação

--- src/main/java/com/guris/trezemaio/dto/ItemDTO.java
+++ src/main/java/com/guris/trezemaio/dto/ItemDTO.java
@@ -1,6 +1,9 @@
 package com.guris.trezemaio.dto;
 
 import com.guris.trezemaio.model.enums.TipoItem;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -13,8 +16,10 @@ public class ItemDTO {
 
     private Long id;
 
+    @NotNull(message = "O tipo do item é obrigatório.")
     private TipoItem type;
 
+    @NotBlank(message = "O título é obrigatório.")
     private String title;
     private String subtitle;
     private int pagesCount;
@@ -23,8 +28,12 @@ public class ItemDTO {
     private LocalDate publicationDate;
 
     private String language;
+
+    @Min(value = 0, message = "A quantidade não pode ser negativa.")
     private int quantity;
     private String observation;
+
+    @NotBlank(message = "O autor é obrigatório.")
     private String autor;
     private String edicao;
     private String localization;
