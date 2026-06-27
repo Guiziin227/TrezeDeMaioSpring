ee0a632eef883f60baa566b0f761e4b71bcd8ba8
OtavioBaggio <otavio.baggiorosul@gmail.com>
2026-06-27 00:57:55 -0300

    Auditoria de editoras

--- src/main/java/com/guris/trezemaio/model/Editora.java
+++ src/main/java/com/guris/trezemaio/model/Editora.java
@@ -3,11 +3,19 @@ package com.guris.trezemaio.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "tb_editora")
+@EntityListeners(AuditingEntityListener.class)
 public class Editora {
 
     @Id
@@ -21,4 +29,20 @@ public class Editora {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "address_id")
     private Endereco address;
+
+    @CreatedDate
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @LastModifiedDate
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @CreatedBy
+    @Column(name = "criado_por")
+    private String criadoPor;
+
+    @LastModifiedBy
+    @Column(name = "modificado_por")
+    private String modificadoPor;
 }
